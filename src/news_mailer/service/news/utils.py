1d561fb9c4c60c40d6635f726e8493f46215dcf5
Forward489 <glennstevensantoso@gmail.com>
2025-08-09 10:18:23 +0700

    fix: fix validation

--- src/news_mailer/service/news/utils.py
+++ src/news_mailer/service/news/utils.py
@@ -11,15 +11,15 @@ logger = get_logger(__name__)
 
 def try_or_return_default_topic_queries() -> dict[str, Any]:
     try:
+        print(getenv("TOPIC_QUERIES"))
         return TopicQueries(**json.loads(getenv("TOPIC_QUERIES"))).model_dump()
     except Exception as exc:
         logger.warning("Failed to parse TOPIC_QUERIES: %s\n%s", exc, "Returning default queries.")
         return TopicQueries(
             macroeconomy="inflation OR GDP OR unemployment OR macroeconomy",
             geopolitics="geopolitics OR geopolitical risk OR foreign policy",
-            us_stock_market="S&P 500 OR Dow Jones OR Nasdaq",
+            stock_market="S&P 500 OR Dow Jones OR Nasdaq OR FTSE OR Nikkei OR DAX OR Hang Seng",
             cryptocurrency="cryptocurrency OR bitcoin OR ethereum",
-            global_stock_markets="FTSE OR Nikkei OR DAX OR Hang Seng",
             commodities="oil OR gold OR copper OR commodity prices",
             technology="artificial intelligence OR generative AI OR open source AI OR blockchain technology",
         ).model_dump()
